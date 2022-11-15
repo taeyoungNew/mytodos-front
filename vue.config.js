ff78c541de9bc78650632cf29003415dd0141a87
taeyoungNew <dndbxhd10@naver.com>
2022-11-15 21:43:30 +0900

    vue.config 추가

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  server: {
+    port: process.env.PORT || 3010,
+  },
 });
