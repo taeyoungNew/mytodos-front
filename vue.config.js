d85bbe60d05116c60e970a4818c075e7f62da7f7
taeyoungNew <dndbxhd10@naver.com>
2023-01-15 12:25:36 +0900

    변경사항 1) 차트 아래의 %테이지 변경 2) 표위에 달성률에 따른 메세지 변경

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: ["vuetify"],
-  server: {
-    port: process.env.PORT || 3010,
-  },
-});
